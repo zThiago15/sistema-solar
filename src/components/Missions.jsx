a1eb94e8d311903a1a9ea5f592a92c1f478ede9a
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-06 17:34:52 -0300

    requisito 10 - renderizando componente MissionCard para cada missao espacial da lista de missoes

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
     return (
-      <>
-        <div data-testid="missions" />
+      <div data-testid="missions">
         <Title headline="Missões" />
-      </>
+        {missions.map((mission) => {
+          const { name, year, country, destination } = mission;
+          return (<MissionCard
+            key={ name }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />);
+        })}
+      </div>
     );
   }
 }
