0fb0eb79a3e6df5d852f6b0a8438539b8e904e2c
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-06 17:05:17 -0300

    requisito 7 - criando componente Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      
+      <>
+        <div data-testid="missions" />
+        <Title />
+      </>
     );
   }
 }
